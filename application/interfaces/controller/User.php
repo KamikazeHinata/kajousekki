fe94f9b8ada2b6a802f4a86980bc4d573f46ae2e
KamikazeHinata <s523455082@gmail.com>
2017-12-04 21:26:04 +0900

    修改了没有参数时报错的bug

--- application/interfaces/controller/User.php
+++ application/interfaces/controller/User.php
@@ -4,17 +4,21 @@ namespace app\interfaces\controller;
 use think\Loader;
 use think\Request;
 
-
 class User
 {
     /**
      * 用户登录
+     * @return int 成功为1，失败为0
      */
     public function login()
     {
         $uModel   = Loader::model("User");
         $userinfo = Request::instance()->param();
-        $result   = $uModel->confirm($userinfo["username"], $userinfo["password"]);
+        if (!empty($userinfo["username"]) && !empty($userinfo["password"])) {
+            $result = $uModel->confirm($userinfo["username"], $userinfo["password"]);
+        } else {
+            $result = 0;
+        }
 
         return $result;
     }
